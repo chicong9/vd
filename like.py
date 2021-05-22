ae6513e0cf9542d747ef0ae77a48e7aa46958abe
võ chí công <chicong14101112@gmail.com>
2021-05-22 22:42:39 +0700

    like

---
+++ like.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from pynput.keyboard import Key, Controller
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+key = Controller()
+i = 0
+option = Options()
+option.add_argument("--disable-notifications")
+driver = webdriver.Chrome("C:/chromedriver_win32/chromedriver.exe",chrome_options=option)
+driver.get("https://www.facebook.com/")
+elem = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]")
+elem.send_keys("congvochi10@yahoo.com.vn")
+time.sleep(2)
+elem= driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/input[1]")
+elem.send_keys("nkdieutrinh2609")
+time.sleep(2)
+elem.send_keys(Keys.RETURN)
+action = ActionChains(driver)
+
+while i < 5:
+    time.sleep(5)
+    key.press('j')
+    key.release('j')
+    key.press('l')
+    key.release('l')
+    key.press(Key.right)
+    key.release(Key.right)
+    key.press(Key.enter)
+    key.release(Key.enter)
+    i = i +1
